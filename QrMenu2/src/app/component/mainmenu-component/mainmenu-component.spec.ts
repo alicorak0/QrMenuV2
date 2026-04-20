0ea95882c550d645e9f12c737743be05c5a4cc7b
birayaltun <149802729+birayaltun@users.noreply.github.com>
2026-04-21 01:21:32 +0300

    21 Nisan 01:21

--- QrMenu2/src/app/component/mainmenu-component/mainmenu-component.spec.ts
+++ QrMenu2/src/app/component/mainmenu-component/mainmenu-component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MainmenuComponent } from './mainmenu-component';
+import { MainMenuComponent } from './mainmenu-component';
 
-describe('MainmenuComponent', () => {
-  let component: MainmenuComponent;
-  let fixture: ComponentFixture<MainmenuComponent>;
+describe('MainMenuComponent', () => {
+  let component: MainMenuComponent;
+  let fixture: ComponentFixture<MainMenuComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MainmenuComponent]
+      imports: [MainMenuComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(MainmenuComponent);
+    fixture = TestBed.createComponent(MainMenuComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
